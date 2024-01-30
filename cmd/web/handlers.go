890cff82dbc0628989f99ed14b65685439493c88
David Moravec <moravec@numsolution.cz>
2024-01-30 14:25:28 +0100

    Add html

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
@@ -12,7 +14,20 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("hello from Snippetbox"))
+	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl")
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Eror", 500)
+	}
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
